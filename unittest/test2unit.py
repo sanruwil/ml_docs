dc082a22a441d0969d746808fcee77b678d69e48
Wilfredy Santamaria Ruiz <sanruwil@gmail.com>
2023-06-15 16:34:41 -0500

    Subir informacion

---
+++ unittest/test2unit.py
@@ -0,0 +1,30 @@
+import unittest
+
+
+import unittest
+
+
+def raises_error(*args, **kwds):
+    raise ValueError('Invalid value: ' + str(args) + str(kwds))
+
+
+class ExceptionTest(unittest.TestCase):
+
+    def testTrapLocally(self):
+        try:
+            raises_error('a', b='c')
+        except ValueError:
+            pass
+        else:
+            self.fail('Did not see ValueError')
+
+    def testAssertRaises(self):
+        self.assertRaises(
+            ValueError,
+            raises_error,
+            'a',
+            b='c',
+        )
+
+if __name__ == '__main__':
+    unittest.main()
